0911407ee4473fe64b05832158ccd08be42c340b
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-19 22:10:17 -0300

    Buscando produtos da API

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
     static navigationOptions = {
         title: "JSHunt",
     };
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    // modelo para o this enxergar a função
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        const { docs } = response.data;
+
+        console.log(docs);
+    }
 
     render() {
         return (
